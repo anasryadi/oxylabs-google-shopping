b08d40d8340a380d094c2d2d960dbd8ad64ce2c7
anasryadi <anasryadi0@gmail.com>
2023-08-14 23:08:41 -0700

    started the form in Header.tsx and added the magnifying glass icon from heroicons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,32 @@
 "use client";
 
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import Link from "next/link";
 
 function Header() {
   return (
     <header>
-        <Link href="/">
-      <Image
-        src="https://links.papareact.com/208"
-        alt="logo"
-        width={150}
-        height={150}
-        className="object-contain mr-10"
-      />
+      <Link href="/">
+        <Image
+          src="https://links.papareact.com/208"
+          alt="logo"
+          width={150}
+          height={150}
+          className="object-contain mr-10"
+        />
       </Link>
+
+      <div>
+        {/* {FORM} */}
+        <form action="">
+            <div>
+                <div>
+                    <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
+                </div>
+            </div>
+        </form>
+      </div>
     </header>
   );
 }

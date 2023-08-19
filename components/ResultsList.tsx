7a40a101c2be5db6ffc0d5ad727c33e9283d2e1b
anasryadi <anasryadi0@gmail.com>
2023-08-18 20:27:19 -0700

    added styling to the item specific filters

--- components/ResultsList.tsx
+++ components/ResultsList.tsx
@@ -1,4 +1,5 @@
 import { PageResult } from "@/typings";
+import Link from "next/link";
 
 type Props = {
   results: PageResult[];
@@ -7,15 +8,25 @@ type Props = {
 
 function ResultsList({ results, term }: Props) {
   return (
-    <div>
+    <div className="flex md:px-5">
       {/* sidebar */}
       <div>
-        {/* each page */}
         {results.map((pageResult) => (
           <div key={pageResult.job_id} className="space-y-2">
-            {pageResult.content.results.filters?.map((filter) => (
-              <div>
-                <p>{filter.name}</p>
+            {pageResult.content.results.filters?.map((filter, i) => (
+              <div key={i} className="border rounded-r-lg md:rounded-lg p-5">
+                <p className="font-bold">{filter.name}</p>
+                <div className="flex flex-col">
+                  <p>{filter.name}</p>
+                  {filter.values.map((value) => (
+                    <Link
+                      prefetch={false}
+                      href={`https://www.google.com/$value.url`}
+                    >
+                      {value.value}
+                    </Link>
+                  ))}
+                </div>
               </div>
             ))}
           </div>
@@ -23,6 +34,13 @@ function ResultsList({ results, term }: Props) {
       </div>
 
       {/* main body */}
+      <div>
+        {results.map((pageResult, i) => (
+            <div key={pageResult.job_id} className="">
+
+            </div>
+        ))}
+      </div>
     </div>
   );
 }

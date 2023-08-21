eb40301dd395d72fa40652144cb3e6fb2c95473d
anasryadi <anasryadi0@gmail.com>
2023-08-21 13:52:30 -0700

    added the title and price and seller of the searched item with styling and also added the loading message that appears during the search

---
+++ app/search/[term]/loading.tsx
@@ -0,0 +1,7 @@
+function LoadingPage() {
+  return (
+    <div>Loading Results...</div>
+  )
+}
+
+export default LoadingPage
--- components/ResultsList.tsx
+++ components/ResultsList.tsx
@@ -34,20 +34,51 @@ function ResultsList({ results, term }: Props) {
       </div>
 
       {/* main body */}
-      <div>
+      <div className="px-5 md:p-10 md:pt-0 space-y-5 flex-1">
         {results.map((pageResult, i) => (
-          <div key={pageResult.job_id} className="">
+          <div
+            key={pageResult.job_id}
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3"
+          >
             {i !== 0 && <hr className="w-full col-span-full" />}
             <div className="md:col-span-2 lg:col-span-3 xl:col-span-4 py-5">
               <div className="flex space-x-2 items-center divide-x-2">
                 <h1>Shop On Google</h1>
-                <h2 className="text-xl font-semibold pl-2">Search Results for Page {i + 1}</h2>
+                <h2 className="text-xl font-semibold pl-2">
+                  Search Results for Page {i + 1}
+                </h2>
               </div>
 
               <h3 className="font-extralight">
                 Showing results for "{decodeURIComponent(term)}"
               </h3>
             </div>
+            {pageResult?.content?.results?.organic?.map((item) => (
+              <Link
+                href={
+                  item.url.includes("url?url=")
+                    ? // send to external URL
+                      item.url.split("url?url=")?.[1]
+                    : // remove any query params and send to Google Shopping page
+                      item.url.split("?")?.[0]
+                }
+                key={item.pos}
+                prefetch={false}
+                className={`border rounded-2xl flex flex-col hover:shadow-lg transition duration-200 ease-in-out ${
+                  item.url.includes("url?url=") && "italic"
+                }`}
+              >
+                <div className="border-b p-5 flex-1">
+                  <p className="text-[#1B66D2]">{item.title}</p>
+                </div>
+                <div className="px-5 py-2 not-italic">
+                  <p className="font-light">
+                    {item.price_str} {item.currency}
+                  </p>
+                  <p className="text-[#1B66D2] font-semibold">{item.merchant.name}</p>
+                </div>
+              </Link>
+            ))}
           </div>
         ))}
       </div>

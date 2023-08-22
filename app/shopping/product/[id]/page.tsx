6147db33330e1ac9ade8065f8305a24e1bd2355e
anasryadi <anasryadi0@gmail.com>
2023-08-21 19:41:49 -0700

    installed react-skeleton and added routes for product data

---
+++ app/shopping/product/[id]/page.tsx
@@ -0,0 +1,20 @@
+import { getFetchUrl } from "@/lib/getFetchUrl";
+import { StarIcon } from "@heroicons/react/24/solid";
+import { notFound } from "next/navigation";
+
+export const revalidate = 300;
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+function ProductPage({ params: { id } }: Props) {
+    // API Endpoint
+
+    
+  return <div>ProductPage</div>;
+}
+
+export default ProductPage;

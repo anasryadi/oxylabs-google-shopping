a99540511ef2f6e0913ba2d0de3c585967b6c3be
anasryadi <anasryadi0@gmail.com>
2023-08-30 21:46:19 -0700

    Added the welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div className="">
-      <h1>Let's build a GOOGLE Shopping clone with Oxylabs</h1>
+    <div className="p-10 pt-0 text-center md:text-left">
+      <h1 className="text-3xl font-extralight mb-5">
+        Welcome to Google Shopping
+      </h1>
+      <h2 className="mb-5">
+        Get started by clicking on one of the example search items or typing in
+        an item yourself above!
+      </h2>
     </div>
   );
 }

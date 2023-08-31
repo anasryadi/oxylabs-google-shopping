f5ff1e79c99c36d189f6834b1f5260c1933947b2
anasryadi <anasryadi0@gmail.com>
2023-08-30 22:10:13 -0700

    added search suggestion boxes to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,38 @@
+import Link from "next/link";
+
+const SEARCHES = [
+  {
+    id: 1,
+    term: "Monitors over $500",
+    url: "/search/monitors?sort_by=r&min_price=500",
+    color: "bg-blue-500",
+  },
+  {
+    id: 2,
+    term: "iPhone 14 Pro Max",
+    url: "/search/iphone 14 pro max",
+    color: "bg-red-500",
+  },
+  {
+    id: 3,
+    term: "Macbook Pro",
+    url: "/search/macbook",
+    color: "bg-yellow-500",
+  },
+  {
+    id: 4,
+    term: "Airpods Pro",
+    url: "/search/airpods",
+    color: "bg-green-500",
+  },
+  {
+    id: 5,
+    term: "Tablets under $300",
+    url: "/search/tablets?sort_by=r&min_price=300",
+    color: "bg-purple-500",
+  },
+];
+
 export default function Home() {
   return (
     <div className="p-10 pt-0 text-center md:text-left">
@@ -8,6 +43,19 @@ export default function Home() {
         Get started by clicking on one of the example search items or typing in
         an item yourself above!
       </h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-center items-center gap-5">
+        {SEARCHES.map((search) => (
+          <Link
+            prefetch={false}
+            key={search.id}
+            href={search.url}
+            className={`${search.color} w-full h-36 hover:opacity-50 text-white font-bold py-2 px-4 rounded`}
+          >
+            {search.term}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }

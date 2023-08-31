43e330957b7eddc02de1a17e02dd682d5ae48783
anasryadi <anasryadi0@gmail.com>
2023-08-30 22:19:57 -0700

    fixed key bug on term loading.tsx

--- app/search/[term]/loading.tsx
+++ app/search/[term]/loading.tsx
@@ -9,7 +9,7 @@ function LoadingPage() {
         <div className="w-36 md:w-64 space-y-5">
           {[
             ...Array(4).map((_, i) => (
-              <div className="border rounded-r-lg md:rounded-lg p-5">
+              <div key={i} className="border rounded-r-lg md:rounded-lg p-5">
                 <p className="font-bold">
                   <Skeleton count={Math.floor(Math.random() * 5) + 1} />
                 </p>
@@ -26,8 +26,8 @@ function LoadingPage() {
                 Scraping Real Results Via Oxylabs!
               </h2>
             </div>
-            {[...Array(10)].map((item) => (
-              <div className="p-5 border rounded-2xl">
+            {[...Array(10)].map((item, i) => (
+              <div key={i} className="p-5 border rounded-2xl">
                 <Skeleton count={2} />
                 <br />
                 <Skeleton count={1} />
